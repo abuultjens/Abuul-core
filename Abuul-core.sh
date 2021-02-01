9a3aea8c9c3fcbce378384736802f3cea7da7d92
Andrew Buultjens <buultjensa@gmail.com>
2021-02-01 18:55:13 +1100

    Update Abuul-core.sh

--- Abuul-core.sh
+++ Abuul-core.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# assign infile and outfile from pos arg
 INFILE=${1}
 OUTFILE=${2}
 
@@ -13,10 +14,9 @@ echo "Making index"
 # make ele_pos index from snps.tab file
 tail -n +2 ${INFILE} | cut -f 1-2 | tr '\t' '_' > ${RAND}_INDEX.txt
 
+echo "Making database"
 # make list of isolates in snps.tab file
 head -1 ${INFILE} | cut -f 4- | tr '\t' '\n' > ${RAND}_isolates_list.txt
-
-echo "Making database"
 # make db from snps.tab from each isolate folder
 for ISOLATE in $(cat ${RAND}_isolates_list.txt); do
 	tail -n +2 ${ISOLATE}/snps.csv | cut -f 1-2 -d ',' | tr ',' '_' >> ${RAND}_index_tmp.txt
@@ -29,13 +29,20 @@ echo "NUMBER_OF_CONSEQUENCES	CONSEQUENCE" > ${RAND}_CONSEQUENCE.txt
 
 echo "Fetching consequences from database"
 for INDEX in $(cat ${RAND}_INDEX.txt); do
+
+	# check if loci is in snps.csv
 	if grep ^"${INDEX}," ${RAND}_DB.txt 1> /dev/null 2>&1; then
+		# count number of different consequences for single loci
 		WC=`grep ^"${INDEX}," ${RAND}_DB.txt | cut -f 10 -d ',' | sort | uniq | wc -l`
+		
+		# if number of consequences is one
                 if [ "${WC}" == "1" ]; then
 			CONSEQUENCE_TMP=`grep ^"${INDEX}," ${RAND}_DB.txt | cut -f 10 -d ',' | sort | uniq | head -1 | tail -1`
 			#ALLELE=`grep ^"${INDEX}," ${RAND}_DB.txt | head -1 | tail -1 | cut -f 4 -d ','`
 			CONSEQUENCE=[CONSEQUENCE:${CONSEQUENCE_TMP}]
                 fi 
+		
+		# if number of consequences is two
 		if [ "${WC}" == "2" ]; then
 			#FIRST_ALLELE=`grep ^"${INDEX}," ${RAND}_DB.txt | head -1 | tail -1 | cut -f 4 -d ','`
 			FIRST_CONSEQUENCE=`grep ^"${INDEX}," ${RAND}_DB.txt | cut -f 10 -d ',' | sort | uniq | head -1 | tail -1`
@@ -43,6 +50,8 @@ for INDEX in $(cat ${RAND}_INDEX.txt); do
 			SECOND_CONSEQUENCE=`grep ^"${INDEX}," ${RAND}_DB.txt | cut -f 10 -d ',' | sort | uniq | head -2 | tail -1`
 			CONSEQUENCE=[CONSEQUENCE:${FIRST_CONSEQUENCE}]_CONSEQUENCE:${SECOND_CONSEQUENCE}]
 		fi
+		
+		# if number of consequences is three
 		if [ "${WC}" == "3" ]; then
                         #FIRST_ALLELE=`grep ^"${INDEX}," ${RAND}_DB.txt | head -1 | tail -1 | cut -f 4 -d ','`
                         FIRST_CONSEQUENCE=`grep ^"${INDEX}," ${RAND}_DB.txt | cut -f 10 -d ',' | sort | uniq | head -1 | tail -1`
@@ -52,6 +61,8 @@ for INDEX in $(cat ${RAND}_INDEX.txt); do
                         THIRD_CONSEQUENCE=`grep ^"${INDEX}," ${RAND}_DB.txt | cut -f 10 -d ',' | sort | uniq | head -3 | tail -1`
 			CONSEQUENCE=[CONSEQUENCE:${FIRST_CONSEQUENCE}]_CONSEQUENCE:${SECOND_CONSEQUENCE}_CONSEQUENCE:${THIRD_CONSEQUENCE}]
 		fi
+		
+		# if number of consequences is four
                 if [ "${WC}" == "4" ]; then
                         #FIRST_ALLELE=`grep ^"${INDEX}," ${RAND}_DB.txt | head -1 | tail -1 | cut -f 4 -d ','`
                         FIRST_CONSEQUENCE=`grep ^"${INDEX}," ${RAND}_DB.txt | cut -f 10 -d ',' | sort | uniq | head -1 | tail -1`
@@ -63,7 +74,8 @@ for INDEX in $(cat ${RAND}_INDEX.txt); do
                         FOURTH_CONSEQUENCE=`grep ^"${INDEX}," ${RAND}_DB.txt | cut -f 10 -d ',' | sort | uniq | head -4 | tail -1`
 			CONSEQUENCE=[CONSEQUENCE:${FIRST_CONSEQUENCE}]_CONSEQUENCE:${SECOND_CONSEQUENCE}_CONSEQUENCE:${THIRD_CONSEQUENCE}_CONSEQUENCE:${FOURTH_CONSEQUENCE}]
                 fi
-
+	
+	# if the loci could not be found in the snps.csv files
 	else
 		CONSEQUENCE=VARIANT_NOT_FOUND_IN_snps.csv
 	fi
@@ -72,7 +84,7 @@ done
 
 paste ${INFILE} ${RAND}_CONSEQUENCE.txt > ${OUTFILE}
 
-
+# rm tmp files
 cp ${RAND}_DB.txt DB.txt
 rm ${RAND}_*
 
